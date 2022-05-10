952a2627e84fa246af1a1e404825412b9515e91d
Jaz-ma <potatoorgy3@gmail.com>
2022-05-10 23:04:55 +0100

    number of posts and recieved likes

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,8 @@ class User extends Authenticatable
     public function likes(){
         return $this->hasMany(Like::class);
     }
+
+    public function recievedLikes(){
+        return $this->hasManyThrough(Like::class,Post::class);
+    }
 }
--- resources/views/user/posts.blade.php
+++ resources/views/user/posts.blade.php
@@ -1,7 +1,14 @@
 <x-layouts.app>
 
-    <div class="flex justify-center">
+<div class="flex justify-center">
 
+    <div class="w-8/12">
+        <div class="p-6">
+            <h1 class="text-2xl font-medium mb-1">
+                {{$user->username}}
+            </h1>
+            <p>posted {{$posts->count()}} {{Str::plural('post',$posts->count())}} and recieved {{$user->recievedLikes->count()}} {{Str::plural('like',$user->recievedLikes->count())}} </p>
+        </div>
         <div class="bg-white p-6 round-lg w-8/12">
         @if ($posts->count())
                 @foreach ($posts as $post )
@@ -14,7 +21,9 @@
             @endif
 
         </div>
-    </div>
+
+     </div>
+</div>
 
 </x-layouts.app>
 
